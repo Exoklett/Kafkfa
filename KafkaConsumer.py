7f8ccd470ba19627eb8692a0d1eac278218e9ff2
DomiS <80386984+Exoklett@users.noreply.github.com>
2023-06-08 20:59:32 +0200

    Add files via upload

---
+++ KafkaConsumer.py
@@ -0,0 +1,44 @@
+from kafka import KafkaConsumer
+from kafka import KafkaConsumer
+from datetime import datetime, timedelta
+
+
+
+
+
+def main():
+    bootstrap_servers = 'localhost:9092'
+    topic_en = "WikiEdits_EN"
+    topic_de = "WikiEdits_DE"
+    
+    Consumer_EN = KafkaConsumer(topic_en, bootstrap_servers=bootstrap_servers)
+    
+    
+    while True:
+    
+        
+        for message in Consumer_EN:
+            input = message.value.decode('utf-8')
+            print(f"Received message: {input}")
+            edits_amounts_en = input
+            
+        Consumer_DE = KafkaConsumer(topic_de, bootstrap_servers=bootstrap_servers)
+        
+        for message in Consumer_DE:
+            input = message.value.decode('utf-8')
+            print(f"Received message: {input}")
+            edits_amounts_de = input
+            
+        
+            # from now on, you could use the retrieved data for further data handling
+            # for example this quick and dirty MySQL approach
+            # conn = mysql.connector.connect(host ="", user ="", ...)
+            # cursor = conn.cursor()
+            # cursor.execute("INSERT INTO example VALUES(edits_amounts_en, ...)")
+            # conn.commit
+            
+        if not Consumer_EN.assignment() & Consumer_DE.assignment() :
+         break   
+if __name__ == "__main__":
+    main()
+    
---
+++ KafkaProducer.py
@@ -0,0 +1,85 @@
+import random
+import requests
+import datetime
+from time import sleep
+from datetime import timedelta
+from kafka import KafkaProducer
+    
+def responseCall_de():
+    urlWikide = "https://de.wikipedia.org/w/api.php"
+    xend = datetime.datetime.now()
+    xend = xend - timedelta(hours=2)
+    xstart = xend - timedelta(minutes=1)
+    
+    paramsde = {
+    "action": "query",
+    "format": "json",
+    "list": "recentchanges",
+    "rclimit": 400,
+    "rcend": xstart.strftime("%Y-%m-%dT%H:%M:%SZ")  
+    }
+    
+    response_de = requests.get(urlWikide, params = paramsde)
+    if response_de.status_code == requests.codes.ok:
+        databuff = response_de.json()
+        return databuff
+    else: print(f"Issue DE: {response_de.status_code}")    
+    
+    
+    
+def responseCall_en():
+    urlWikien = "https://en.wikipedia.org/w/api.php"
+    xend = datetime.datetime.now()
+    xend = xend - timedelta(hours=2)
+    xstart = xend - timedelta(minutes=1)
+    
+    paramsde = {
+    "action": "query",
+    "format": "json",
+    "list": "recentchanges",
+    "rclimit": 400,
+    "rcend": xstart.strftime("%Y-%m-%dT%H:%M:%SZ")  
+    }
+    
+    response_en = requests.get(urlWikien, params = paramsde)
+    if response_en.status_code == requests.codes.ok:
+        databuff = response_en.json()
+        return databuff
+    else: print(f"Issue EN: {response_en.status_code}")
+    
+    
+    
+    
+def main():
+
+        
+    bootstrap_servers = 'localhost:9092'
+    topic_en = "WikiEdits_EN"
+    topic_de = "WikiEdits_DE"
+    producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+    
+    
+    for i in range (20):   
+        databuff_en = responseCall_en()
+        databuff_de = responseCall_de()  
+        # changesen = databuff_en["query"]["recentchanges"]
+        # num_en = str(len(changesen))
+        # changesde = databuff_de["query"]["recentchanges"]
+        # num_de = str(len(changesde))
+        
+        # here is a quick and simple approach:
+        # 2 topics for every needed argument or data/input stream
+        # has the advantage of a seperated data access
+        
+        sleep(random.randrange(0,1))
+        # producer.send(topic_en, value=num_en.encode('utf-8'))
+        # producer.send(topic_de, value=num_de.encode('utf-8'))
+        producer.send(topic_en,value=databuff_en)
+     
+    producer.flush()
+    producer.close()
+        
+    
+if __name__ == "__main__":
+    main()
+    
